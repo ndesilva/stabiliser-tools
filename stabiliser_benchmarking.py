27ec547834b2b7a806c055533c137452324251f0
WilfredSalmon <was29@cam.ac.uk>
2023-10-28 19:38:30 -0700

    added stabiliser benchmarking

---
+++ stabiliser_benchmarking.py
@@ -0,0 +1,12 @@
+
+'''Given a randomly generated stabiliser state (generate A, l, q, glboal phase at random), check how long it takes to accept'''
+def accepts_random_stabiliser_state():
+    pass
+
+'''Given a radomly generated state (generate random vector and normalise), check how long it takes to reject'''
+def rejects_random_state():
+    pass
+
+'''Given a random stabiliser state, break it in one entry, check how long it takes to reject'''
+def rejects_almost_stabiliser():
+    pass
