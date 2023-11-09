f3416221dcc4093bc4130333a73dd4ceb7faeac4
WilfredSalmon <was29@cam.ac.uk>
2023-11-09 13:11:18 -0800

    added test TODOs

--- F2_helper/F2_helper.py
+++ F2_helper/F2_helper.py
@@ -28,11 +28,11 @@ def imag_mod2product(x : int, y : int) -> complex:
 def fast_log2(x : int) -> int:
     return x.bit_length() - 1
 
-def get_bit_at(x : int, bit_location: int) -> int:
+def get_bit_at(x : int, bit_location: int) -> int: # TODO test
     power_of_two = 1 << bit_location
 
     return (x & power_of_two) == power_of_two
 
-def get_vector_expansion(dimension : int, basis_vectors : list[int], coefficients : int) -> int:
+def get_vector_expansion(dimension : int, basis_vectors : list[int], coefficients : int) -> int: # TODO test
     terms = [get_bit_at(coefficients, j)*basis_vectors[j] for j in range(dimension)]
     return functools.reduce(lambda x,y : x ^ y, terms, 0)
