fixed precision error

pauli/Pauli.py
@@ -52,7 +52,7 @@ class Pauli: # TODO add from_matrix method
             
         factor = self.phase * state_vector[index] * f2.sign_mod2product(self.z_vector, index) / state_vector[index ^ self.x_vector]
 
-        match factor:
+        match np.round(factor,5):
             case 1:
                 bit = 0
             case -1:

pauli/pauli_check.py
@@ -30,7 +30,7 @@ def is_pauli(matrix : np.ndarray, allow_global_factor = True) -> bool:
 
         entry = matrix[col ^ q, col]/phase
 
-        match entry:
+        match np.round(entry,5):
             case 1:
                 pass
             case -1:
@@ -50,7 +50,7 @@ def is_pauli(matrix : np.ndarray, allow_global_factor = True) -> bool:
     return True
 
 def is_valid_pauli_entry(entry : float) -> bool:
-    match entry:
+    match np.round(entry,5):
         case 1:
             return True
         case -1:
@@ -63,7 +63,7 @@ def is_valid_pauli_entry(entry : float) -> bool:
             return False
 
 def is_pm_one(entry : float) -> bool:
-    match entry:
+    match np.round(entry,5):
         case 1:
             return True
         case -1:

stabiliser_state/stabiliser_from_state_vector.py
@@ -82,7 +82,7 @@ class Stabiliser_From_State_Vector: #TODO currently assumes length 2^n
 
         # get linear terms
         for index in weight_one_bitstrings:
-            match non_zero_coeffs[index]/self.first_entry:
+            match np.round(non_zero_coeffs[index]/self.first_entry, 5):
                 case 1:
                     pass
                 case -1:
@@ -109,7 +109,7 @@ class Stabiliser_From_State_Vector: #TODO currently assumes length 2^n
 
                 value = non_zero_coeffs[index]/(self.first_entry*linear_part)
 
-                match value:
+                match np.round(value,5):
                     case 1:
                         pass
                     case -1:
@@ -145,7 +145,7 @@ class Stabiliser_From_State_Vector: #TODO currently assumes length 2^n
         return True
 
 def is_valid_stabiliser_entry(entry : complex) -> bool:
-    match entry:
+    match np.round(entry, 5):
         case 1:
             return True
         case -1: