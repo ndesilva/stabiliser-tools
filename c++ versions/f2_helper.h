f47fc395fa6bd8326e891d38623aa2959106f6c9
WilfredSalmon <was29@cam.ac.uk>
2024-04-23 15:14:13 +0100

    minor refactor

--- c++ versions/f2_helper.h
+++ c++ versions/f2_helper.h
@@ -9,11 +9,11 @@ namespace fst {
 /// as integers)
 int f2_dot_product(int x, int y);
 
-/// Gives (-1)^(x . y), where . is the F_2 inner product between 2
+/// Gives (-1)^(x.y), where . is the F_2 inner product between 2
 /// F_2 vectors (represented as integers)
 int sign_f2_dot_product(int x, int y);
 
-/// Gives (i)^(x . y), where . is the F_2 inner product between 2
+/// Gives (i)^(x.y), where . is the F_2 inner product between 2
 /// F_2 vectors (represented as integers)
 std::complex<int> imag_f2_dot_product(int x, int y);
 
