30057f61e3544e887eb45922e5ec6dc50eba9e97
blackninja9939 <blackninja9939@users.noreply.github.com>
2024-05-03 19:09:08 +0200

    Use explicit std::size_t and size_t literals instead of deduced int types. Also sprinkly more const and explicit casts.

--- src/stabiliser_state/stabiliser_state.cpp
+++ src/stabiliser_state/stabiliser_state.cpp
@@ -1,48 +1,56 @@
 #include "stabiliser_state.h"
 #include "f2_helper/f2_helper.h"
 
-#include <math.h>
-#include <iostream>
+#include <cmath>
 
-using namespace fst;
-
-Stabiliser_State::Stabiliser_State(const int number_qubits, const int dim) 
-    : number_qubits(number_qubits), dim(dim) {}
-
-Stabiliser_State::Stabiliser_State(const int number_qubits) 
-    : number_qubits(number_qubits) {
-        dim = number_qubits;
+namespace fst
+{
+    Stabiliser_State::Stabiliser_State( const std::size_t number_qubits, const std::size_t dim )
+        : number_qubits( number_qubits )
+        , dim( dim )
+    {
     }
 
-std::vector<std::complex<float>> Stabiliser_State::get_state_vector() const {
-    int support_size = 1 << dim;
-    std::complex<float> factor = global_phase / float(sqrt(support_size));
-    
-    std::vector<std::complex<float>> state_vector (1 << number_qubits, 0);
-
-    for(int vector_index = 0; vector_index < support_size; vector_index++) {
-        int whole_space_index = shift ^ evaluate_basis_expansion(vector_index);
-        std::complex<float> phase = get_phase(vector_index);
-        state_vector[whole_space_index] = factor*phase;
+    Stabiliser_State::Stabiliser_State( const std::size_t number_qubits )
+        : Stabiliser_State(number_qubits, number_qubits )
+    {
     }
 
-    return state_vector;  
-}
+    std::vector<std::complex<float>> Stabiliser_State::get_state_vector() const
+    {
+		const std::size_t support_size = 1z << dim;
+        const std::complex<float> factor = global_phase / float( std::sqrt( support_size ) );
 
-size_t Stabiliser_State::evaluate_basis_expansion(const size_t vector_index) const {
-    int result = 0;
+		std::vector<std::complex<float>> state_vector( 1z << number_qubits, 0 );
 
-    for(int j = 0; j < dim; j++){
-        result ^= basis_vectors[j]*( (1<<j & vector_index) == 1 << j );
+        for ( std::size_t vector_index = 0; vector_index < support_size; vector_index++ )
+        {
+            const std::size_t whole_space_index = shift ^ evaluate_basis_expansion( vector_index );
+            std::complex<float> phase = get_phase( vector_index );
+            state_vector[ whole_space_index ] = factor * phase;
+        }
+
+        return state_vector;
     }
 
-    return result;
-}
+    std::size_t Stabiliser_State::evaluate_basis_expansion( const std::size_t vector_index ) const
+    {
+        std::size_t result = 0;
 
-std::complex<float> Stabiliser_State::get_phase(const size_t vector_index) const {
-    float real_linear = sign_f2_dot_product(vector_index, real_linear_part);
-    std::complex<float> imag_linear = imag_f2_dot_product(vector_index, imaginary_part); 
-    float real_quadratic = evaluate_quadratic_form(vector_index, quadratic_form);
+        for ( std::size_t j = 0; j < dim; j++ )
+        {
+            result ^= basis_vectors[ j ] * ( ( 1z << j & vector_index ) == 1z << j );
+        }
 
-    return real_linear*imag_linear*real_quadratic;
+        return result;
+    }
+
+    std::complex<float> Stabiliser_State::get_phase( const std::size_t vector_index ) const
+    {
+        const float real_linear = static_cast<float>( sign_f2_dot_product( vector_index, real_linear_part ) );
+        const std::complex<float> imag_linear = imag_f2_dot_product( vector_index, imaginary_part );
+        const float real_quadratic = static_cast<float>( evaluate_quadratic_form( vector_index, std::span( quadratic_form ) ) );
+
+        return real_linear * imag_linear * real_quadratic;
+    }
 }
