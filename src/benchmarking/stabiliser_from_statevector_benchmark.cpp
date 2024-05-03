ecfeb66db1ce8cf8ca86e0c9cb4dbe54cddca492
WilfredSalmon <was29@cam.ac.uk>
2024-05-03 22:49:39 +0100

    first benchmarking

--- src/benchmarking/stabiliser_from_statevector_benchmark.cpp
+++ src/benchmarking/stabiliser_from_statevector_benchmark.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <complex>
+#include <chrono>
+#include <fstream>
 
 #include "./stim/circuit_vs_amplitudes.h"
+#include "stabiliser_state/stabiliser_state_from_statevector.h"
+
+using namespace std::chrono;
 
 std::vector<std::complex<float>> get_last_basis_vector(int number_qubits) {
     size_t vector_length = 1 << number_qubits;
@@ -35,14 +40,42 @@ std::vector<std::complex<float>> get_non_stabiliser_state(int number_qubits) {
 }
 
 int main() {
-    std::cout << "Testing Stim\n";
+    std::cout << "###\nBenchmarking Stim\n###\n";
 
-    bool basis_result = stim::stabilizer_state_vector_to_circuit(get_last_basis_vector(10));
-    bool uniform_result = stim::stabilizer_state_vector_to_circuit(get_uniform_superposition(10));
-    bool non_stab_result = stim::stabilizer_state_vector_to_circuit(get_non_stabiliser_state(10));
+    std::vector<std::vector<std::complex<float>>> statevectors;
 
-    std::cout << "Basis Vector: " << basis_result << std::endl;
-    std::cout << "Uniform: " << uniform_result << std::endl;
-    std::cout << "Non Stabiliser: " << non_stab_result << std::endl;
+    statevectors.push_back(get_last_basis_vector(10));
+    statevectors.push_back(get_uniform_superposition(10));
+    statevectors.push_back(get_non_stabiliser_state(10));
+
+    for (const auto &vector : statevectors) {
+        auto start_time = high_resolution_clock::now();
+
+        bool result = stim::stabilizer_state_vector_to_circuit(vector);
+
+        auto end_time = high_resolution_clock::now();
+
+        auto time_taken = duration_cast<microseconds>(end_time - start_time);
+
+        std::cout << "Stim Time taken: " << time_taken.count() << " microseconds" << std::endl;
+        std::cout << "Result was: " << result << std::endl;
+    }
+
+    std::cout << "###\nBenchmarking Our Method\n###\n";
+
+    for (const auto &vector : statevectors) {
+        auto start_time = high_resolution_clock::now();
+
+        fst::Stabiliser_From_Vector_Convertor convertor (vector);
+
+        bool result = convertor.is_stabiliser_state;
+
+        auto end_time = high_resolution_clock::now();
+
+        auto time_taken = duration_cast<microseconds>(end_time - start_time);
+
+        std::cout << "Our Time taken: " << time_taken.count() << " microseconds" << std::endl;
+        std::cout << "Result was: " << result << std::endl;
+    }
     return 0;
 }
