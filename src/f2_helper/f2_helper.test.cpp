fcdebcca2bd95c51aab8bec7cafb1c56b83adfa5
blackninja9939 <blackninja9939@users.noreply.github.com>
2024-05-03 19:12:30 +0200

    Use binary literals.

--- src/f2_helper/f2_helper.test.cpp
+++ src/f2_helper/f2_helper.test.cpp
@@ -2,52 +2,60 @@
 
 #include "f2_helper.h"
 
+#include <array>
+
 using namespace fst;
 
-TEST_CASE("mod 2 product","[f2 helper]"){
-    size_t x = 26; // 11010
-    size_t y = 19; // 10011
-    size_t z = 3;  // 00011
+TEST_CASE( "mod 2 product", "[f2 helper]" )
+{
+	const std::size_t x = 0b11010;
+	const std::size_t y = 0b10011;
+	const std::size_t z = 0b00011;
 
-    REQUIRE(f2_dot_product(x,y) == 0);
-    REQUIRE(f2_dot_product(x,z) == 1);
+	REQUIRE( f2_dot_product( x, y ) == 0 );
+	REQUIRE( f2_dot_product( x, z ) == 1 );
 }
 
-TEST_CASE("signed mod 2 product", "[f2 helper]") {
-    size_t x = 51; // 110011
-    size_t y = 3;  // 000011
-    size_t z = 49; // 110101
+TEST_CASE( "signed mod 2 product", "[f2 helper]" )
+{
+	const std::size_t x = 0b110011;
+	const std::size_t y = 0b000011;
+	const std::size_t z = 0b110101;
 
-    REQUIRE(sign_f2_dot_product(x,y) == 1);
-    REQUIRE(sign_f2_dot_product(x,z) == -1);
+	REQUIRE( sign_f2_dot_product( x, y ) == 1 );
+	REQUIRE( sign_f2_dot_product( x, z ) == -1 );
 }
 
-TEST_CASE("imaginary mod 2 product", "[f2 helper]") {
-    size_t x = 43; // 101011
-    size_t y = 3;  // 000011
-    size_t z = 45; // 101101
+TEST_CASE( "imaginary mod 2 product", "[f2 helper]" )
+{
+	const std::size_t x = 0b101011;
+	const std::size_t y = 0b000011;
+	const std::size_t z = 0b101101;
 
-    REQUIRE(imag_f2_dot_product(x,y) == float(1));
-    REQUIRE(imag_f2_dot_product(x,z) == std::complex<float> (0, 1));
+	REQUIRE( imag_f2_dot_product( x, y ) == 1.0f );
+	REQUIRE( imag_f2_dot_product( x, z ) == std::complex<float>( 0, 1 ) );
 }
 
-TEST_CASE("evaluate quadratic form", "[f2 helper]") {
-    SECTION("dimension 3") {
-        std::vector<size_t> quadratic_form = {3, 6}; // x_0 x_1 + x_1 x_2
+TEST_CASE( "evaluate quadratic form", "[f2 helper]" )
+{
+	SECTION( "dimension 3" )
+	{
+		const std::array<std::size_t, 2> quadratic_form = { 3, 6 }; // x_0 x_1 + x_1 x_2
 
-        size_t point_1 = 3; // 011
-        size_t point_2 = 7; // 111
+		const std::size_t point_1 = 0b011;
+		const std::size_t point_2 = 0b111;
 
-        REQUIRE(evaluate_quadratic_form(point_1, quadratic_form) == -1);
-        REQUIRE(evaluate_quadratic_form(point_2, quadratic_form) == 1);
-    }
+		REQUIRE( evaluate_quadratic_form( point_1, std::span( quadratic_form ) ) == -1 );
+		REQUIRE( evaluate_quadratic_form( point_2, std::span( quadratic_form ) ) == 1 );
+	}
 
-    SECTION("dimension 4") {
-        std::vector<size_t> quadratic_form = {3, 6, 9}; // x_0 x_1 + x_1 x_2 + x_0 x_3
-        size_t point_1 = 15; // 1111
-        size_t point_2 = 11; // 1011  
+	SECTION( "dimension 4" )
+	{
+		const std::array<std::size_t, 3> quadratic_form = { 3, 6, 9 }; // x_0 x_1 + x_1 x_2 + x_0 x_3
+		const std::size_t point_1 = 0b1111;
+		const std::size_t point_2 = 0b1011;
 
-        REQUIRE(evaluate_quadratic_form(point_1, quadratic_form) == -1);
-        REQUIRE(evaluate_quadratic_form(point_2, quadratic_form) == 1);
-    }
+		REQUIRE( evaluate_quadratic_form( point_1, std::span( quadratic_form ) ) == -1 );
+		REQUIRE( evaluate_quadratic_form( point_2, std::span( quadratic_form ) ) == 1 );
+	}
 }
