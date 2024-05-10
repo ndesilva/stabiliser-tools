093ec6dadc72119f33751cd1931ffd7cab5b49de
WilfredSalmon <was29@cam.ac.uk>
2024-05-10 14:21:10 +0100

    added implementations for pauli class

--- src/pauli/pauli.h
+++ src/pauli/pauli.h
@@ -14,20 +14,21 @@ namespace fst
 
         bool sign_bit = 0;
         bool imag_bit = 0;
+        std::complex<float> phase = 1;
 
-        Pauli (const int number_qubits, const std::size_t x_vector, const std::size_t z_vector, const bool sign_bit, const bool imag_bit);
+        Pauli(const int number_qubits, const std::size_t x_vector, const std::size_t z_vector, const bool sign_bit, const bool imag_bit);
 
         void update_phase();
-
-        std::vector<std::vector<std::complex<float>>> get_matrix() const;
-
-        void multiply_by_pauli_on_right(const Pauli &other_pauli);
-        std::vector<std::complex<float>> multiply_vector (const std::complex<float> &vector) const;
 
         bool is_hermitian() const;
         
         bool commutes_with(const Pauli &other_pauli) const;
-        bool anti_commutes_with(const Pauli &other_pauli) const;
+        bool anticommutes_with(const Pauli &other_pauli) const;
+
+        std::vector<std::vector<std::complex<float>>> get_matrix() const;
+        std::vector<std::complex<float>> multiply_vector (const std::complex<float> &vector) const;
+
+        void multiply_by_pauli_on_right(const Pauli &other_pauli);
     };
 }
 
