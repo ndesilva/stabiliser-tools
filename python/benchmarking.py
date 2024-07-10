cc7d7e8d63acea1d41b7b775317169492e7adc2a
MrLatinNerd <minghuayin1729@gmail.com>
2024-07-09 18:01:11 -0700

    Add benchmarking files

---
+++ python/benchmarking.py
@@ -0,0 +1,85 @@
+import numpy as np
+import time
+import pickle
+from benchmarking.Benchmarking_Data import Benchmarking_Data
+import benchmarking.generators as gs
+import cProfile
+
+import clifford.Clifford as c
+import benchmarking.brute_force.clifford_check as bf_cc
+import clifford.clifford_from_matrix as cc
+
+import stabiliser_state.stabiliser_from_state_vector as ssc
+
+reps = int(5)
+
+min_qubits = 1
+max_qubits = 12
+qubit_numbers = list(range(min_qubits, max_qubits + 1))
+
+profile = cProfile.Profile()
+
+def time_function_with_generator(function_to_time, generator) -> np.ndarray:
+    times = np.zeros(max_qubits)
+    
+    for n in qubit_numbers:
+        print(f'n is {n}')
+        timer = 0
+
+        for j in range(reps):
+            print(j)
+            input = generator(n)
+
+            st = time.perf_counter()
+            # profile.enable()
+            
+            function_to_time(input)
+            
+            # profile.disable()
+            et = time.perf_counter()
+
+            timer += et-st
+
+        times[n-1] = timer/reps
+    
+    return times
+
+def our_method_clifford(matrix : np.ndarray) -> bool:
+    return cc.Clifford_From_Matrix(matrix, only_testing = True).is_clifford
+
+def our_method_stabiliser_state(state : np.ndarray) -> bool:
+    return ssc.Stabiliser_From_State_Vector(state).is_stab_state
+
+functions_to_time = [bf_cc.is_clifford, our_method_clifford] 
+function_strings = ['brute force', 'our method']
+
+generation_types = [gs.random_clifford]
+generation_strings = ['random clifford']
+
+pre_string = 'testing C1'
+
+num_functions = len(functions_to_time)
+num_generators = len(generation_types)
+
+assert num_functions == len(function_strings)
+assert num_generators == len(generation_strings) 
+
+base_filestring = './benchmarking/data'
+
+for function_index in range(num_functions):
+    for generation_index in range(num_generators):
+        function_string = function_strings[function_index]
+        generation_string = generation_strings[generation_index]
+        
+        filename = f'{base_filestring}/{pre_string} {function_string} on {generation_string}.npy'
+
+        print(f'timing {function_string} with {generation_string}')
+
+        times = time_function_with_generator(functions_to_time[function_index], generation_types[generation_index])
+
+        data = Benchmarking_Data(function_string, generation_string, qubit_numbers, times)
+
+        with open(filename, 'wb') as fl:
+            pickle.dump(data, fl)
+
+# profile.dump_stats('./logs/numba_with_compound_ifs.log')
---
+++ python/benchmarking/generators.py
@@ -0,0 +1,75 @@
+import numpy as np
+import random
+from pauli.Pauli import Pauli
+import benchmarking.generator_dependencies.randstab as rs
+import scipy.stats as sts
+import qiskit.quantum_info as qi
+
+def random_unitary(n : int) -> np.ndarray:
+    return sts.unitary_group.rvs(1<<n)
+
+def random_pauli_matrix(n : int) -> np.ndarray:
+    s = random.randrange(2)
+    t = random.randrange(2)
+    p = random.randrange(1<<n)
+    q = random.randrange(1<<n)
+
+    return Pauli(n, p, q, s, t).generate_matrix()
+
+def random_almost_pauli_matrix(n : int) -> np.ndarray:
+    matrix = random_pauli_matrix(n)
+    modify_random_matrix_entry(n, matrix)
+
+    return matrix
+
+
+def random_vector(n : int) -> np.ndarray:
+    unnormalised = np.random.rand(1<<n) + 1j*np.random.rand(1<<n)
+    return unnormalised / np.sqrt(unnormalised @ unnormalised.conj())
+
+def random_stab_state(n : int) -> np.ndarray:
+    return rs.random_stabilizer_state(n)
+
+def random_almost_stab_state(n : int) -> np.ndarray:
+    stab_state = random_stab_state(n)
+
+    i = random.randrange( 1 << n )
+
+    if stab_state[i]:
+        if random.randrange(2):
+            stab_state[i] *= 1j
+        else:
+            stab_state[i] = 0
+    else:
+        stab_state[i] = 1
+
+    return stab_state
+
+def random_clifford(n : int) -> np.ndarray:
+    return qi.random_clifford(n).to_matrix()
+
+def random_almost_clifford(n : int) -> np.ndarray:
+    matrix = random_clifford(n)
+    
+    if random.randrange(2):
+        modify_random_matrix_entry(n, matrix)
+    else:
+        modify_random_column(n, matrix)
+
+    return matrix
+
+def modify_random_matrix_entry(n : int, matrix : np.ndarray) -> None:
+    i = random.randrange(1 << n)
+    j = random.randrange(1 << n)
+
+    if matrix[i,j]:
+        if random.randrange(2):
+            matrix[i,j] *= 1j
+        else:
+            matrix[i,j] = 0
+    else:
+        matrix[i,j] = 1
+
+def modify_random_column(n : int, matrix : np.ndarray) -> None:
+    j = random.randrange(1 << n)
+    matrix[:, j] *= 1j
