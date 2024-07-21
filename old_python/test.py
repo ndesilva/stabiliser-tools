e33c4d6e003d6e7fb5e9fed9b0ef48fb378ed169
WilfredSalmon <was29@cam.ac.uk>
2024-07-21 12:32:48 +0100

    removed old python files

--- old_python/test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-loader = unittest.TestLoader()
-suite = loader.discover('.', '*_test.py')
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
