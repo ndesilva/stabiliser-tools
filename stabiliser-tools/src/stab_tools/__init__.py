db9b05e3a55df41a01a1102fcc950b63ba8cc464
MrLatinNerd <minghuayin1729@gmail.com>
2024-08-20 17:01:25 -0700

    Make multiple Linux versions

--- stabiliser-tools/src/stab_tools/__init__.py
+++ stabiliser-tools/src/stab_tools/__init__.py
@@ -1,8 +1,15 @@
 import os
 import sys
-PATH_TO_LIBRARY = f'{os.path.abspath(os.path.dirname(__file__))}'
+PATH_TO_LIBRARY = os.path.abspath(os.path.dirname(__file__))
+print(f'Adding {PATH_TO_LIBRARY} to path if necessary')
 if PATH_TO_LIBRARY not in sys.path:
-    # print(f'Adding {PATH_TO_LIBRARY} to path')
     sys.path.extend([PATH_TO_LIBRARY])
+try:
+    if PATH_TO_LIBRARY not in os.environ['LD_LIBRARY_PATH']:
+        raise ImportError
+except (KeyError, ImportError):
+    print('\nWARNING: You may be about to receive an ImportError. (If you don\'t, you can safely ignore this message.) '
+          'To resolve this, try running the following command in your terminal:\n'
+          f'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:{PATH_TO_LIBRARY}\n', file=sys.stderr)
 from fast import *
 del os, sys, PATH_TO_LIBRARY
