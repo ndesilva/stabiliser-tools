01dd1a489ece8f06ab71baf6762d62557f0df63a
MrLatinNerd <minghuayin1729@gmail.com>
2024-09-01 19:19:40 -0700

    Testing GitHub Actions automation

--- stabiliser-tools/src/stab_tools/__init__.py
+++ stabiliser-tools/src/stab_tools/__init__.py
@@ -1,15 +1,19 @@
 import os
 import sys
+
 PATH_TO_LIBRARY = os.path.abspath(os.path.dirname(__file__))
 print(f'Adding {PATH_TO_LIBRARY} to path if necessary')
 if PATH_TO_LIBRARY not in sys.path:
     sys.path.extend([PATH_TO_LIBRARY])
-try:
-    if PATH_TO_LIBRARY not in os.environ['LD_LIBRARY_PATH']:
-        raise ImportError
-except (KeyError, ImportError):
-    print('\nWARNING: You may be about to receive an ImportError. (If you don\'t, you can safely ignore this message.) '
-          'To resolve this, try running the following command in your terminal:\n'
-          f'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:{PATH_TO_LIBRARY}\n', file=sys.stderr)
+
+if sys.platform == 'linux':
+    try:
+        if PATH_TO_LIBRARY not in os.environ['LD_LIBRARY_PATH']:
+            raise ImportError
+    except (KeyError, ImportError):
+        print('\nWARNING: You may be about to receive an ImportError. (If you don\'t, you can safely ignore this message.) '
+            'To resolve this, try running the following command in your terminal:\n'
+            f'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:{PATH_TO_LIBRARY}\n', file=sys.stderr)
+
 from fast import *
 del os, sys, PATH_TO_LIBRARY
